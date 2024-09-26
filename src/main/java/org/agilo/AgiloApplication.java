3d5931fcb26db5c668ca3294449402cb68d72a0d
Sumit Mansukhlal Savaliya <savaliyasumit717@gmail.com>
2024-09-25 22:36:26 -0300

    Initial setup

---
+++ src/main/java/org/agilo/AgiloApplication.java
@@ -0,0 +1,13 @@
+package org.agilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgiloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/agilo/AgiloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
