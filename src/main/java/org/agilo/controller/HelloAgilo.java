72c4c5d31387b4b19a64342038e00e1a3816c9a9
Sumit Mansukhlal Savaliya <savaliyasumit717@gmail.com>
2024-09-25 23:30:07 -0300

    adds sample api docs for test route

--- src/main/java/org/agilo/controller/HelloAgilo.java
+++ src/main/java/org/agilo/controller/HelloAgilo.java
@@ -1,12 +1,22 @@
 package org.agilo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
+@RequestMapping("/api/v1")
 public class HelloAgilo {
-    @GetMapping("/hello-agilo")
+    @Operation(summary = "Greeting Endpoint", description = "Returns a simple greeting message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/hello")
     public String helloAgilo() {
-        return "Hello";
+        return "Hello from Agilo!";
     }
 }
