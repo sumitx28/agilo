0cd2531578ed375d44c46d000dfd7e86be3b2b0c
Sumit Mansukhlal Savaliya <savaliyasumit717@gmail.com>
2024-09-30 22:24:30 -0300

    refactor open api config

--- src/main/java/org/agilo/configuration/OpenAPIConfiguration.java
+++ src/main/java/org/agilo/configuration/OpenAPIConfiguration.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,21 +12,33 @@ import java.util.List;
 
 @Configuration
 public class OpenAPIConfiguration {
+
+    @Value("${openapi.server-url}")
+    private String SERVER_URL;
+
+    @Value("${openapi.version}")
+    private String API_VERSION;
+
+    private final String SERVER_DESCRIPTION = "development";
+    private final String CONTACT_NAME = "Agilo Admin";
+    private final String CONTACT_EMAIL = "sumit.savaliya@dal.ca";
+    private final String API_TITLE = "A Modern Day Task Management App";
+    private final String API_DESCRIPTION = "This API exposes endpoints to manage tasks.";
+
     @Bean
     public OpenAPI defineOpenApi() {
         Server server = new Server();
-        server.setUrl("http://localhost:8080");
-        server.setDescription("Development");
+        server.setUrl(SERVER_URL);
+        server.setDescription(SERVER_DESCRIPTION);
 
         Contact myContact = new Contact();
-        myContact.setName("Agilo Admin");
-        myContact.setEmail("sumit.savaliya@dal.ca");
+        myContact.setName(CONTACT_NAME);
+        myContact.setEmail(CONTACT_EMAIL);
 
         Info information = new Info()
-                .title("A Modern Day Task Management App")
-                .version("1.0")
-                .description("This API exposes endpoints to manage tasks.")
-                .contact(myContact);
+                .title(API_TITLE)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION);
 
         return new OpenAPI().info(information).servers(List.of(server));
     }
